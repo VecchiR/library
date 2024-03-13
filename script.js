feb67d414b036a312b898440b74101811135c9b7
Vecchi <79600745+VecchiR@users.noreply.github.com>
2024-03-13 16:54:03 -0300

    Create page's basic structure and functionalities
    Consists of:
    - A table with 2 example rows already filled;
    - A button to add new books
        (for the time being, via prompts for each value, but this will be
        changed to make use of dialogs and modals);
    - A delete button for each row
        (for now, it just alerts "DELETED". Later on, it will delete the
        entire row)

---
+++ script.js
@@ -0,0 +1,64 @@
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = function () {
+        return (`${title} by ${author}, ${pages} pages, ${read}`)
+    };
+}
+
+const addBookButton = document.querySelector('.add-book');
+addBookButton.addEventListener('click', promptUser);
+
+const booksTable = document.querySelector('.books-table');
+
+let bookDeleteButton;
+
+
+function promptUser() {
+    let title = prompt('title');
+    let author = prompt('author');
+    let pages = parseInt(prompt('pages'));
+    let read = prompt('read');
+    const newBook = new Book(title, author, pages, read);
+    addBookToLibrary(newBook);
+}
+
+function addBookToLibrary(book) {
+    console.log(book);
+    myLibrary.push(book);
+    let newRow = document.createElement("tr");
+    for (let x = 0; x < 4; x++) {
+        let newData = document.createElement("td");
+        let newText = document.createTextNode(Object.values(book)[x]);
+        newData.appendChild(newText);
+        newRow.appendChild(newData);
+    }
+    let newDeleteBtn = document.createElement("button");
+    newDeleteBtn.addEventListener('click', test);
+    let deleteBtnText = document.createTextNode("Delete");
+    newDeleteBtn.appendChild(deleteBtnText);
+    newRow.appendChild(newDeleteBtn);
+    booksTable.appendChild(newRow);
+}
+
+function examplesOnLoad() {
+    const meditations = new Book('Meditations', 'Marcus Aurelius', 208, 'no');
+    addBookToLibrary(meditations);
+    const fahrenheit = new Book ('Fahrenheit 451', 'Ray Bradbury', 159, 'yes');
+    addBookToLibrary(fahrenheit);
+}
+
+
+
+examplesOnLoad();
+
+
+
+
+function test() {
+    alert('DELETED');
+}
